0d639293d087e068c32b538c32bb6d28574bc884
janbugel <janbugel11@gmail.com>
2024-01-17 10:04:19 -0500

    changes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,5 @@
 // navbar.js
-
+/*
 
 // for github.io
 document.addEventListener("DOMContentLoaded", function () {
@@ -7,20 +7,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const navbarContent = `
         <ul class="navbar">
             <li><a href="./">home</a></li>
-            <li><a href="./about">about</a></li>
-            <li><a href="./projects">projects</a></li>
-            <li><a href="./contact">contact</a></li>
-            <li><a href="./education">education</a></li>
+            <li><a href="./about.html">about</a></li>
+            <li><a href="./work.html">work</a></li>
+            <li><a href="./education.html">education</a></li>
+        <li><a href="./contact.html">contact</a></li>
         </ul>
     `;
 
     // Set the content of the placeholder with the navbar content
     document.getElementById("navbar-placeholder").innerHTML = navbarContent;
 });
-
+*/
 
 // for localhost
-/*
+
 
 // navbar.js
 
@@ -40,4 +40,3 @@ document.addEventListener("DOMContentLoaded", function () {
     // Set the content of the placeholder with the navbar content
     document.getElementById("navbar-placeholder").innerHTML = navbarContent;
 });
-*/
