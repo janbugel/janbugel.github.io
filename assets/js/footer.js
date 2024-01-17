5f2c505cbed1a2f53d4e0a93b0d6bbce6debb5af
janbugel <janbugel11@gmail.com>
2024-01-17 09:51:11 -0500

    cleaned up

---
+++ assets/js/footer.js
@@ -0,0 +1,11 @@
+// footer.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Define the footer HTML content as a string
+    const footerContent = `
+        <p>&copy; 2022 John Doe</p>
+    `;
+
+    // Set the content of the footer element with the footer content
+    document.getElementById("footer-placeholder").innerHTML = footerContent;
+});
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,6 @@
 // navbar.js
 
+// for github.io
 document.addEventListener("DOMContentLoaded", function () {
     // Define the navbar HTML content as a string
     const navbarContent = `
@@ -15,3 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
     // Set the content of the placeholder with the navbar content
     document.getElementById("navbar-placeholder").innerHTML = navbarContent;
 });
+
+// for localhost
+
+/*
+// navbar.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Define the navbar HTML content as a string
+    const navbarContent = `
+        <ul class="navbar">
+            <li><a href="./">home</a></li>
+            <li><a href="./about.html">about</a></li>
+            <li><a href="./projects.html">projects</a></li>
+            <li><a href="./contact.html">contact</a></li>
+            <li><a href="./education.html">education</a></li>
+        </ul>
+    `;
+
+    // Set the content of the placeholder with the navbar content
+    document.getElementById("navbar-placeholder").innerHTML = navbarContent;
+});
+*/
