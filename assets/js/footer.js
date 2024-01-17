0eaa955325e881c0ed54585d39aab0db5aa4d26b
janbugel <janbugel11@gmail.com>
2024-01-17 10:35:27 -0500

    asdf

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Define the footer HTML content as a string
     const footerContent = `
-        <p>&copy; 2023 John Doe | <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer">Twitter</a> |
-         <a href="https://www.linkedin.com/in/yourlinkedinprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+        <p>&copy; 2023 John Doe |
+        <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer">twitter</a> |
+        <a href="https://www.linkedin.com/in/yourlinkedinprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a><p>
     `;
 
     // Set the content of the footer element with the footer content
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,38 +1,14 @@
 // navbar.js
-/*
-
-// for github.io
-document.addEventListener("DOMContentLoaded", function () {
-    // Define the navbar HTML content as a string
-    const navbarContent = `
-        <ul class="navbar">
-            <li><a href="./">home</a></li>
-            <li><a href="./about.html">about</a></li>
-            <li><a href="./work.html">work</a></li>
-            <li><a href="./education.html">education</a></li>
-        <li><a href="./contact.html">contact</a></li>
-        </ul>
-    `;
-
-    // Set the content of the placeholder with the navbar content
-    document.getElementById("navbar-placeholder").innerHTML = navbarContent;
-});
-*/
-
-// for localhost
-
-
-// navbar.js
 
 document.addEventListener("DOMContentLoaded", function () {
     // Define the navbar HTML content as a string
     const navbarContent = `
         <ul class="navbar">
-            <li><a href="./">home</a></li>
-            <li><a href="./about.html">about</a></li>
-            <li><a href="./work.html">work</a></li>
-            <li><a href="./education.html">education</a></li>
-            <li><a href="./contact.html">contact</a></li>
+            <li><a href="/">home</a></li>
+            <li><a href="about">about</a></li>
+            <li><a href="work">work</a></li>
+            <li><a href="education">education</a></li>
+            <li><a href="contact">contact</a></li>
             
         </ul>
     `;
