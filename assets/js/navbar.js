68329e32ac44b1ebc8b0ceb95f171f4bb7bb3b44
janbugel <janbugel11@gmail.com>
2024-01-17 09:34:40 -0500

    navbar js added

---
+++ assets/js/navbar.js
@@ -0,0 +1,17 @@
+// navbar.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Define the navbar HTML content as a string
+    const navbarContent = `
+        <ul class="navbar">
+            <li><a href="./">home</a></li>
+            <li><a href="./about">about</a></li>
+            <li><a href="./projects">projects</a></li>
+            <li><a href="./contact">contact</a></li>
+            <li><a href="./education">education</a></li>
+        </ul>
+    `;
+
+    // Set the content of the placeholder with the navbar content
+    document.getElementById("navbar-placeholder").innerHTML = navbarContent;
+});
