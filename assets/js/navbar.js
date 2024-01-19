64e903cb12bcdd437fce833c36339fbe11473889
janbugel <janbugel11@gmail.com>
2024-01-20 00:01:29 +0100

    change

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,28 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const navbarContent = `
-        <ul class="navbar">
-            <li><a href="/">home</a></li>
-            <li><a href="work">work</a></li>
-            <li><a href="education">education</a></li>
-            <li><a href="contact">contact</a></li>
-            <li><a href="javascript:void(0);" id="colorPickerLabel" class="color-picker-label" onclick="openColorPicker()">
-             <label for="colorPicker">themes</label>
-        </a></li>
-        <input type="color" id="colorPicker" style="display: none;">
-    `;
-
-    document.getElementById("navbar-placeholder").innerHTML = navbarContent;
-
-    // color picker
-
     class ColorPickerApp {
         constructor(colorPickerId) {
             this.colorPicker = document.getElementById(colorPickerId);
             this.loadColor();
             this.addEventListeners();
         }
-    
+
         loadColor() {
             const savedColor = localStorage.getItem("selectedColor");
             if (savedColor) {
@@ -30,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 this.colorPicker.value = savedColor;
             }
         }
-    
+
         addEventListeners() {
             this.colorPicker.addEventListener("input", () => {
                 const selectedColor = this.colorPicker.value;
@@ -38,23 +22,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 localStorage.setItem("selectedColor", selectedColor);
             });
         }
-    
+
         updateBackgroundColor(color) {
             document.body.style.backgroundColor = color;
             const darkerColor = this.calculateDarkerColor(color);
-    
+
             // Update header background color
             const header = document.querySelector('header');
             if (header) {
                 header.style.backgroundColor = darkerColor;
             }
-    
+
             // Update navbar background color
             const nav = document.querySelector('nav');
             if (nav) {
                 nav.style.backgroundColor = darkerColor;
             }
-    
+
             const brightness = this.calculateBrightness(color);
             const textColor = (brightness < 128) ? "#ffffff" : "#000000";
             document.body.style.color = textColor;
@@ -63,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 link.style.color = textColor;
             });
         }
-    
+
         calculateDarkerColor(hexColor) {
             const rgb = this.hexToRgb(hexColor);
             const darkerRgb = {
@@ -73,12 +57,12 @@ document.addEventListener("DOMContentLoaded", function () {
             };
             return this.rgbToHex(darkerRgb);
         }
-    
+
         calculateBrightness(hexColor) {
             const rgb = this.hexToRgb(hexColor);
             return (rgb.r * 299 + rgb.g * 587 + rgb.b * 114) / 1000;
         }
-    
+
         hexToRgb(hex) {
             hex = hex.replace(/^#/, '');
             const bigint = parseInt(hex, 16);
@@ -87,15 +71,13 @@ document.addEventListener("DOMContentLoaded", function () {
             const b = bigint & 255;
             return { r, g, b };
         }
-    
+
         rgbToHex(rgb) {
             return "#" + ((1 << 24) + (rgb.r << 16) + (rgb.g << 8) + rgb.b).toString(16).slice(1);
         }
     }
-    function initializeApp() {
-        const colorPickerApp = new ColorPickerApp("colorPicker");
-    }
-    initializeApp();
+
+    // Initialize the app
+    const colorPickerApp = new ColorPickerApp("colorPicker");
+
 });
-
-
