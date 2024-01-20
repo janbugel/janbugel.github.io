6045e1d80807a038903629d1b7060aac5c9babc3
janbugel <janbugel11@gmail.com>
2024-01-20 12:12:41 +0100

    cleanup

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
         <p>&copy; 2024 John Doe |
         <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer">twitter</a> |
         <a href="https://www.linkedin.com/in/yourlinkedinprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
-
-
     `;
 
     // Set the content of the footer element with the footer content
--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -7,6 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
             <li><a href="work">work</a></li>
             <li><a href="education">education</a></li>
             <li><a href="contact">contact</a></li>
+            <li>
+            <a href="javascript:void(0);" id="colorPickerLabel" class="color-picker-label" onclick="openColorPicker()">
+                <label id="colorPickerLabel" for="colorPicker">themes</label>
+            </a>
+            </li>
+            <input type="color" id="colorPicker" style="display: none;">
+        </ul>
     `;
 
     document.getElementById("navbar-placeholder").innerHTML = navbarContent;
