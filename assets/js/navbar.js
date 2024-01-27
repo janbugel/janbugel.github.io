ea409899bcf8e0e8d5850278e3b063d7c79a0e72
janbugel <janbugel11@gmail.com>
2024-01-27 21:21:22 +0100

    asdf

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,60 +1,38 @@
+// Define the html template tag
+function html(strings, ...values) {
+  let result = strings[0];
+  for (let i = 0; i < values.length; i++) {
+    result += values[i] + strings[i + 1];
+  }
+  return result;
+}
+
 // navigation menu
-
 document.addEventListener("DOMContentLoaded", function () {
-  const navbarContent = `
-        <ul class="navbar">
-            <li><a href="/">home</a></li>
-            <li><a href="work">work</a></li>
-            <li><a href="education">education</a></li>
-            <li><a href="contact">contact</a></li>
-
-            <!--
-            https://act-rules.github.io/pages/examples/visible/
-            -->
-            
-            <!--
-            <li>
-                <a href="javascript:void(0);" class="color-picker-label" onclick="onClick()">
-                <label id="colorPickerLabel" for="colorPicker">themes</label>
-                <input type="color" id="colorPicker" style="transform: scale(0%)"></a>
-            </li>
-            -->
-            
-            <li><a href="javascript:void(0);" id="colorPickerLabel" class="color-picker-label" onclick="openColorPicker()">
-            <label id="colorPickerLabel" for="colorPicker">themes</label>
-            </a></li>
-            <input type="color" id="colorPicker" style="transform: scale(0%)""> 
-            
-            <!--
-            <li>
-            <a href="javascript:void(0);" class="color-picker-label" onclick="onClick()"></a>
-            </li>
-
-            <label id="colorPickerLabel" for="colorPicker">themes</label>
-
-            <br>
-
-            <input type="color" id="colorPicker" >
-            -->
-            
-            <!--
-            <label 
-                id="colorPickerLabel" for="colorPicker" tabindex="0">
-                themes
-                <input type="color" id="colorPicker" style="position: absolute; left: -9999px;">
-            </label>
-            -->
-            
-            <!--
-            <li>
-                <a href="javascript:void(0);" class="color-picker-label" onclick="onClick()">
-                <label id="colorPickerLabel" for="colorPicker">themes</label>
-                <input type="color" id="colorPicker" style="position: absolute; left: -9999px;">
-            </a>
-            </li>
-            -->
-        </ul>
-    `;
+  const navbarContent = html`
+    <ul class="navbar">
+      <li><a href="/">home</a></li>
+      <!--
+      <li><a href="work">work</a></li>
+      -->
+      <li><a href="education">education</a></li>
+      <li><a href="contact">contact</a></li>
+      <li>
+        <a
+          href="javascript:void(0);"
+          id="colorPickerLabel"
+          class="color-picker-label"
+          onclick="openColorPicker()"
+        >
+          <label id="colorPickerLabel" for="colorPicker">themes</label>
+        </a>
+      </li>
+      <input type="color" id="colorPicker" style="transform: scale(0%)">
+      <!--
+      https://act-rules.github.io/pages/examples/visible/
+      -->
+    </ul>
+  `;
 
   document.getElementById("navbar-placeholder").innerHTML = navbarContent;
   document.getElementById("theme-toggle-button").click();
