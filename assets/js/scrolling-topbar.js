c2eef4e98a9ee52c86eecf97bd8cfe2853e2f50b
janbugel <janbugel11@gmail.com>
2024-02-21 18:10:54 +0100

    mobile bug fix?

--- assets/js/scrolling-topbar.js
+++ assets/js/scrolling-topbar.js
@@ -1,18 +1,23 @@
 let lastScrollTop = 0;
 let ticking = false;
+let scrollDelay;
 
 function onScroll() {
-  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  clearTimeout(scrollDelay);
 
-  if (currentScroll > lastScrollTop) {
-    // Scroll down
-    document.getElementById("header").classList.add("header-hidden");
-  } else {
-    // Scroll up
-    document.getElementById("header").classList.remove("header-hidden");
-  }
+  scrollDelay = setTimeout(function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+    if (currentScroll > lastScrollTop) {
+      // Scroll down
+      document.getElementById("header").classList.add("header-hidden");
+    } else {
+      // Scroll up
+      document.getElementById("header").classList.remove("header-hidden");
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+  }, 100); // Adjust the delay time (in milliseconds) as needed
 }
 
 window.addEventListener("scroll", onScroll, { passive: true });
