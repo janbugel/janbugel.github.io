01716c6125bd7511997315ca55c4cc13d5c8fa76
janbugel <janbugel11@gmail.com>
2024-02-21 22:23:18 +0100

    better scrolling

--- assets/js/scrolling-topbar.js
+++ assets/js/scrolling-topbar.js
@@ -1,19 +1,28 @@
-var prevScrollpos = window.pageYOffset;
+var prevScrollpos = window.scrollY;
+var scrollDownThreshold = 50; 
+var scrollUpThreshold = 10; 
+
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.scrollY;
 
-  // Check if the scroll is within the page bounds
   if (currentScrollPos > 0 && currentScrollPos < document.body.offsetHeight - window.innerHeight) {
-    var header = document.getElementById("header"); // Get the header element
-    var headerHeight = header.offsetHeight; // Get the dynamic height of the header
+    var header = document.getElementById("header");
+    var headerHeight = header.offsetHeight;
 
-    if (prevScrollpos > currentScrollPos) {
-      header.style.top = "0";
-    } else {
-      // Use the negative of headerHeight to hide the header
-      header.style.top = "-" + headerHeight + "px";
+    // Determine the direction of the scroll
+    var isScrollingDown = prevScrollpos < currentScrollPos;
+    var isScrollingUp = prevScrollpos > currentScrollPos;
+    
+    // Determine if the scroll distance exceeds the threshold for the direction of scroll
+    var exceedsDownThreshold = isScrollingDown && (currentScrollPos - prevScrollpos >= scrollDownThreshold);
+    var exceedsUpThreshold = isScrollingUp && (prevScrollpos - currentScrollPos >= scrollUpThreshold);
+
+    if (exceedsDownThreshold) {
+      header.style.top = "-" + headerHeight + "px"; // Hide the header
+      prevScrollpos = currentScrollPos;
+    } else if (exceedsUpThreshold) {
+      header.style.top = "0"; // Show the header
+      prevScrollpos = currentScrollPos;
     }
   }
-
-  prevScrollpos = currentScrollPos;
 }
