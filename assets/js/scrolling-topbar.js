d920272380fe043df535b226fbfb8e2168231d9e
janbugel <janbugel11@gmail.com>
2024-02-21 18:33:07 +0100

    asdf

--- assets/js/scrolling-topbar.js
+++ assets/js/scrolling-topbar.js
@@ -1,23 +1,19 @@
 let lastScrollTop = 0;
 let ticking = false;
-let scrollDelay;
+const desiredOffset = 100; // Adjust this value based on your desired offset
 
 function onScroll() {
-  clearTimeout(scrollDelay);
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-  scrollDelay = setTimeout(function() {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScroll > lastScrollTop && currentScroll > desiredOffset) {
+    // Scroll down
+    document.getElementById("header").classList.add("header-hidden");
+  } else {
+    // Scroll up
+    document.getElementById("header").classList.remove("header-hidden");
+  }
 
-    if (currentScroll > lastScrollTop) {
-      // Scroll down
-      document.getElementById("header").classList.add("header-hidden");
-    } else {
-      // Scroll up
-      document.getElementById("header").classList.remove("header-hidden");
-    }
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
-  }, 100); // Adjust the delay time (in milliseconds) as needed
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
 }
 
 window.addEventListener("scroll", onScroll, { passive: true });
